d711111a8e44ce1c9156777a533f0d00ce0417ec
dukthem <128906013+dukthem@users.noreply.github.com>
2026-08-11 12:00:40 +0530

    Time: 26 ms (1.33%), Space: 44.7 MB (6.31%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int ans = 0;
+        int last = 0;
+        for (int i = 0; i < gain.length; i++){
+            int curr = last + gain[i];
+            System.out.println(curr);
+            System.out.println(last);
+            ans = Math.max(curr, ans);
+            System.out.println(ans);
+            last = curr;
+        }
+        return ans;
+    }
+}
